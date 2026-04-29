c129eb5f5caaf5ad5ee1d49d1dfaf47ae0380b37
Molly <himollyh4@gmail.com>
2026-04-29 13:52:45 -0400

    Added for the blue planet...my code LUKA HINT HINT

--- practice/input.js
+++ practice/input.js
@@ -1,39 +1,124 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-let x = 0;
-let y = 0;
+    bluePlanetHow2Play = [
+        "Oh No! Looks like the page is at",
+        "the bottom of the ocean!!",
+        "You need to go swim down there",
+        "and get it...make sure to dodge",
+        " the fishing rods!",
+        " Or else you will get caught"
+    ]
+    
+    let starData = [];
 
-let vx = 1;
-let vy = 2;
-let gravity = 1;
-
-function animate(){
-	ctx.clearRect(0,0,800,800);
-	
-	ctx.fillRect(x,y,50,50);
-	requestAnimationFrame(animate);
+function drawRect(x, y, w, h, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, w, h);
 }
 
-animate();
-
-
-
-//event handler
-function handleKeyDown(e){
-	e.preventDefault();
-	if (e.key === "ArrowDown"){
-		y = y + vy;
-	} else if (e.key === "ArrowUp") {
-		y = y - vy
-	} else if (e.key === "ArrowRight") {
-		x = x + vx
-	} else if (e.key === "ArrowLeft") {
-		x = x - vx
-	}
-
+function drawHalfCircle(x, y, r, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI);
+    ctx.fill();
 }
 
-//event listener
-document.addEventListener('keydown', handleKeyDown);
-	
+function write(x, y, text, size, color='white', align='left', font=mainFont) {
+    ctx.textAlign = align;
+    ctx.font = size + "px " + font;
+    ctx.fillStyle = color;
+    ctx.fillText(text, x, y);
+}
+
+
+
+function drawBackground(color) {
+    drawRect(0, 0, canvas.width, canvas.height, color);
+}
+
+function bubbles(){
+    for (let i = 0; i < 100; i++) {
+        starData.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * (canvas.height + 200),
+            size: 1 + 2 * Math.random(),
+        });
+    }
+}
+
+
+function how2Play(){
+    drawRect(700, 200, 400, 400, "#5dacbd");
+    write(785, 250, "How To Play", 40, color='#e0ebeb', align='left', font = "mainFont");
+    write(785, 290, bluePlanetHow2Play[0], 20, color='#e0ebeb', align='left', font = "mainFont");
+    write(785, 330, bluePlanetHow2Play[1], 20, color='#e0ebeb', align='left', font = "mainFont");
+    write(785, 370, bluePlanetHow2Play[2], 20, color='#e0ebeb', align='left', font = "mainFont");
+    write(785, 410, bluePlanetHow2Play[3], 20, color='#e0ebeb', align='left', font = "mainFont");
+    write(785, 450, bluePlanetHow2Play[4], 20, color='#e0ebeb', align='left', font = "mainFont");
+    write(785, 490, bluePlanetHow2Play[5], 20, color='#e0ebeb', align='left', font = "mainFont");
+
+    drawRect(780, 500, 255, 60, "e0ebeb");
+    write(860, 545, "Start", 40, color="#5dacbd", align='left', font = "mainFont");
+}
+
+function drawBoats(){
+    drawHalfCircle(200, 200, 100, "brown");
+    drawRect(180, 50, 30, 220, "brown");
+    drawRect(200, 50, 100, 70, "black");
+
+    drawHalfCircle(800, 200, 100, "brown");
+    drawRect(780, 50, 30, 220, "brown");
+    drawRect(800, 50, 100, 70, "black");
+}
+
+function drawCat() {
+    let x = player.x;
+    let y = player.y - 90;
+
+    // head
+    drawCircle(x, y-20, 35, "orange");
+
+    // body
+    drawCircle(x, y+40, 50, "orange");
+
+    // ears
+    ctx.fillStyle = "orange";
+    ctx.beginPath();
+    ctx.moveTo(x-35, y-45 +20);
+    ctx.lineTo(x-20, y-75);
+    ctx.lineTo(x-10, y-45);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(x+10, y-45);
+    ctx.lineTo(x+20, y-75);
+    ctx.lineTo(x+35, y-45 +20);
+    ctx.fill();
+
+    // eyes
+    drawCircle(x-15, y-20, 4, "black");
+    drawCircle(x+15, y-20, 4, "black");
+
+    // nose
+    // drawCircle(x, y-5, 3, "black");
+
+    // mouth
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.moveTo(x-5, y-10);
+    ctx.lineTo(x,   y-15);
+    ctx.lineTo(x+5, y-10);
+    ctx.stroke();
+
+    // reference point
+    // drawRect(player.x-2, player.y-2, 4, 4, 'black');
+}
+
+
+drawBackground("#24527a");
+drawRect(0, 0, 1200, 300, "white");
+drawBoats();
+how2Play();
+bubbles();
