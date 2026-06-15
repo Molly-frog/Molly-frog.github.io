90507ba250bd37586e46ebabc24bf781cab9f3f9
Molly <himollyh4@gmail.com>
2026-06-15 11:56:13 -0400

    updates

--- portfolio/js/validate.js
+++ portfolio/js/validate.js
@@ -4,3 +4,37 @@
   script.async = false;
   document.head.appendChild(script);
 })();
+
+
+const carouselItems = document.querySelectorAll('.carousel-item');
+const prevButton = document.querySelector('.btn-prev');
+const nextButton = document.querySelector('.btn-next');
+
+let currentIndex = 0; 
+
+function changeSlide(newIndex) {
+  document.querySelector('.carousel-item.active').classList.remove('active');
+  
+  carouselItems[newIndex].classList.add('active');
+  
+  currentIndex = newIndex;
+}
+
+nextButton.addEventListener('click', () => {
+  let nextIndex = currentIndex + 1;
+  
+  if (nextIndex >= carouselItems.length) {
+    nextIndex = 0;
+  }
+  changeSlide(nextIndex);
+});
+
+prevButton.addEventListener('click', () => {
+  let prevIndex = currentIndex - 1;
+  
+  if (prevIndex < 0) {
+    prevIndex = carouselItems.length - 1;
+  }
+  changeSlide(prevIndex);
+});
+
