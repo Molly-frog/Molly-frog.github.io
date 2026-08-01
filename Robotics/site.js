3e0f3a39b3c3b922b13175b7eccbe5d0fd54152c
Molly Horner <himollyh4@gmail.com>
2026-07-31 21:10:08 -0400

    about us half way done trust

---
+++ Robotics/site.js
@@ -0,0 +1,10 @@
+// const items = document.querySelectorAll('.accordion-item');
+
+// items.forEach(item => {
+//   item.addEventListener('click', () => {
+//     // Remove active class from previous item
+//     document.querySelector('.accordion-item.active')?.classList.remove('active');
+//     // Add to the clicked item
+//     item.classList.add('active');
+//   });
+// });
